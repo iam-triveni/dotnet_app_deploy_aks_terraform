be535ac16811bf67dfc38760418c47fa9af23b83
iam-triveni <trivenimaale28@gmail.com>
2025-09-25 09:42:07 -0400

    Update providers.tf

--- scripts/terraform/aks/providers.tf
+++ scripts/terraform/aks/providers.tf
@@ -1,12 +1,12 @@
 provider "azurerm" {
-    subscription_id = "606e824b-aaf7-4b4e-9057-b459f6a4436d"
+    subscription_id = "xxxxxxxxxxxxxxxxxxx"
   features {}
 }
 
 terraform {
   backend "azurerm" {
-    subscription_id = "606e824b-aaf7-4b4e-9057-b459f6a4436d"
-    tenant_id = "104e77d4-81e7-4c16-ab44-935220bed6dd"
+    subscription_id = "xxxxxxxxxxxxxxxxxxxx"
+    tenant_id = "xxxxxxxxxxxxxxxxxx"
     resource_group_name = "trivenirg"
     storage_account_name = "storage234323456"
     container_name = "ktfstate"
